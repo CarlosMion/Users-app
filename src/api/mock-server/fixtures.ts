d84d7ffbe7589ccce6f042c6c8b675ce602881d2
cmion-rbi <cmion@rbi.com>
2022-04-24 22:10:03 -0300

    added mock server for testing endpoints

---
+++ src/api/mock-server/fixtures.ts
@@ -0,0 +1,156 @@
+export const mockUsers = {
+  total_count: 453,
+  incomplete_results: false,
+  items: [
+    {
+      login: 'mion',
+      avatar_url: 'https://avatars.githubusercontent.com/u/2159350?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mioncgp',
+      avatar_url: 'https://avatars.githubusercontent.com/u/60220093?v=4',
+      type: 'User',
+    },
+    {
+      login: 'miono',
+      avatar_url: 'https://avatars.githubusercontent.com/u/856633?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionisation',
+      avatar_url: 'https://avatars.githubusercontent.com/u/4151196?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mion00',
+      avatar_url: 'https://avatars.githubusercontent.com/u/4510647?v=4',
+      type: 'User',
+    },
+    {
+      login: 'Mionger',
+      avatar_url: 'https://avatars.githubusercontent.com/u/33119492?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionova',
+      avatar_url: 'https://avatars.githubusercontent.com/u/23038561?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionuuun',
+      avatar_url: 'https://avatars.githubusercontent.com/u/41195985?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mions1',
+      avatar_url: 'https://avatars.githubusercontent.com/u/32306514?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mion666459',
+      avatar_url: 'https://avatars.githubusercontent.com/u/60477693?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionescu',
+      avatar_url: 'https://avatars.githubusercontent.com/u/218766?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mion-madiori',
+      avatar_url: 'https://avatars.githubusercontent.com/u/19612391?v=4',
+      type: 'User',
+    },
+    {
+      login: 'Mioni',
+      avatar_url: 'https://avatars.githubusercontent.com/u/6830783?v=4',
+      type: 'User',
+    },
+    {
+      login: 'Mion354D',
+      avatar_url: 'https://avatars.githubusercontent.com/u/7450570?v=4',
+      type: 'User',
+    },
+    {
+      login: 'miong',
+      avatar_url: 'https://avatars.githubusercontent.com/u/5671486?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionick',
+      avatar_url: 'https://avatars.githubusercontent.com/u/5993073?v=4',
+      type: 'User',
+    },
+    {
+      login: 'Mionder',
+      avatar_url: 'https://avatars.githubusercontent.com/u/49869397?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionu',
+      avatar_url: 'https://avatars.githubusercontent.com/u/9989413?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionce16',
+      avatar_url: 'https://avatars.githubusercontent.com/u/23311714?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionikwang',
+      avatar_url: 'https://avatars.githubusercontent.com/u/14246708?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mion10',
+      avatar_url: 'https://avatars.githubusercontent.com/u/9251018?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionker',
+      avatar_url: 'https://avatars.githubusercontent.com/u/24206582?v=4',
+      type: 'User',
+    },
+    {
+      login: 'miont',
+      avatar_url: 'https://avatars.githubusercontent.com/u/8156349?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionao0',
+      avatar_url: 'https://avatars.githubusercontent.com/u/33567828?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mion93',
+      avatar_url: 'https://avatars.githubusercontent.com/u/60077260?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionch',
+      avatar_url: 'https://avatars.githubusercontent.com/u/5399851?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionsan',
+      avatar_url: 'https://avatars.githubusercontent.com/u/2951439?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionut',
+      avatar_url: 'https://avatars.githubusercontent.com/u/5769957?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionks27',
+      avatar_url: 'https://avatars.githubusercontent.com/u/63938070?v=4',
+      type: 'User',
+    },
+    {
+      login: 'mionut15',
+      avatar_url: 'https://avatars.githubusercontent.com/u/56038055?v=4',
+      type: 'User',
+    },
+  ],
+};
